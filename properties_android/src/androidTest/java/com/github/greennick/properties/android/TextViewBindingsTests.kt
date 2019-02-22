7dc014cb3d0d95571a7d2a737592ac525d645589
Pavlo Grynyk <paul.grynyk@gmail.com>
2019-02-22 23:18:50 +0200

    - fill test cases for TextView bindings;

--- properties_android/src/androidTest/java/com/github/greennick/properties/android/TextViewBindingsTests.kt
+++ properties_android/src/androidTest/java/com/github/greennick/properties/android/TextViewBindingsTests.kt
@@ -1,6 +1,5 @@
 package com.github.greennick.properties.android
 
-import android.content.Context
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import android.widget.TextView
@@ -19,21 +18,73 @@ import org.junit.Before
  */
 @RunWith(AndroidJUnit4::class)
 class TextViewBindingsTests {
-    lateinit var context: Context
+    private lateinit var textView: TextView
 
     @Before
     fun initContext() {
-        context = InstrumentationRegistry.getTargetContext()
+        textView = TextView(InstrumentationRegistry.getTargetContext())
     }
 
     @Test
     fun textView_text_changed_after_binding() {
         val initValue = "Hello!"
         val property = propertyOf(initValue)
-
-        val textView = TextView(context)
         textView.bindText(property)
 
-        assertEquals(textView.text.toString(), initValue)
+        assertEquals(textView.string, initValue)
     }
+
+    @Test
+    fun textView_text_changing() {
+        val property = propertyOf("Hello!")
+        textView.bindText(property)
+
+        val newValue = "World"
+        property.value = newValue
+        assertEquals(textView.string, newValue)
+    }
+
+    @Test
+    fun textView_stops_changing_after_unsubscribe() {
+        val initValue = "Hello"
+        val property = propertyOf(initValue)
+        val subscription = textView.bindText(property)
+
+        subscription.unsubscribe()
+        val newValue = "World"
+        property.value = newValue
+
+        assertNotEquals(textView.string, newValue)
+        assertEquals(textView.string, initValue)
+    }
+
+    @Test
+    fun textView_changes_property() {
+        val initValue = "Hello"
+        val property = propertyOf(initValue)
+
+        textView.bindTextBidirectionally(property)
+
+        val enteredValue = "World"
+        textView.text = enteredValue
+
+        assertEquals(property.value, enteredValue)
+    }
+
+    @Test
+    fun property_stops_changing_after_unsubscribe() {
+        val property = propertyOf("Hello")
+
+        val subscription = textView.bindTextBidirectionally(property)
+
+        val enteredValue = "World"
+        textView.text = enteredValue
+
+        subscription.unsubscribe()
+        textView.text = "!!!"
+
+        assertEquals(property.value, enteredValue)
+    }
+
+    private val TextView.string get() = text.toString()
 }
