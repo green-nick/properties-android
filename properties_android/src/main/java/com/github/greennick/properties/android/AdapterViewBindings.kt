f1326eceac927213fe688a971ccfbd2b8afd9f0d
greennick <paul.grynyk@gmail.com>
2019-04-16 20:12:10 +0300

    - move out AdapterView bindings;

---
+++ properties_android/src/main/java/com/github/greennick/properties/android/AdapterViewBindings.kt
@@ -0,0 +1,26 @@
+package com.github.greennick.properties.android
+
+import android.view.View
+import android.widget.AdapterView
+import com.github.greennick.properties.generic.MutableProperty
+import com.github.greennick.properties.subscriptions.Subscription
+
+fun AdapterView<*>.bindSelectionBidirectionally(property: MutableProperty<Int>): Subscription {
+    val subscription = property.subscribe {
+        if (it >= 0 && adapter.count >= it) setSelection(it)
+    }
+
+    onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            property.value = position
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            property.value = -1
+        }
+    }
+    subscription.onUnsubscribe {
+        onItemSelectedListener = null
+    }
+    return subscription
+}
--- properties_android/src/main/java/com/github/greennick/properties/android/ViewBindings.kt
+++ properties_android/src/main/java/com/github/greennick/properties/android/ViewBindings.kt
@@ -3,8 +3,6 @@
 package com.github.greennick.properties.android
 
 import android.view.View
-import android.widget.*
-import com.github.greennick.properties.generic.MutableProperty
 import com.github.greennick.properties.generic.Property
 import com.github.greennick.properties.subscriptions.Subscription
 
@@ -13,23 +11,3 @@ fun View.bindVisibility(property: Property<Boolean>): Subscription =
 
 fun View.bindEnabled(property: Property<Boolean>): Subscription =
     property.subscribe(::setEnabled)
-
-fun AdapterView<*>.bindSelectionBidirectionally(property: MutableProperty<Int>): Subscription {
-    val subscription = property.subscribe {
-        if (it >= 0 && adapter.count >= it) setSelection(it)
-    }
-
-    onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-        override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-            property.value = position
-        }
-
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            property.value = -1
-        }
-    }
-    subscription.onUnsubscribe {
-        onItemSelectedListener = null
-    }
-    return subscription
-}
