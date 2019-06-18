75349af9f30f6716187f74e79920d4498440aa85
Pavlo Grynyk <paul.grynyk@gmail.com>
2019-06-18 14:45:45 +0300

    - add extensions which can use findViewById by themselves;

--- properties_android/src/main/java/com/github/greennick/properties/android/TextViewBindings.kt
+++ properties_android/src/main/java/com/github/greennick/properties/android/TextViewBindings.kt
@@ -1,5 +1,6 @@
 package com.github.greennick.properties.android
 
+import android.app.Activity
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.TextView
@@ -8,13 +9,13 @@ import com.github.greennick.properties.generic.Property
 import com.github.greennick.properties.subscriptions.ListenableSubscription
 import com.github.greennick.properties.subscriptions.Subscription
 
-fun <T> TextView.bindText(property: Property<T>): ListenableSubscription =
+fun TextView.bindText(property: Property<out Any?>): ListenableSubscription =
     property.subscribe { text = it?.toString().orEmpty() }
 
 fun TextView.bindTextId(property: Property<Int>): ListenableSubscription =
     property.subscribe { setText(it) }
 
-fun TextView.bindHint(property: Property<out CharSequence>): ListenableSubscription =
+fun TextView.bindHint(property: Property<out CharSequence?>): ListenableSubscription =
     property.subscribe { hint = it }
 
 fun TextView.bindHintId(property: Property<Int>): ListenableSubscription =
@@ -42,3 +43,18 @@ fun TextView.bindTextBidirectionally(property: MutableProperty<String>): Subscri
     subscription.onUnsubscribe { removeTextChangedListener(watcher) }
     return subscription
 }
+
+fun Activity.bindText(id: Int, property: Property<out Any?>): ListenableSubscription =
+    findViewById<TextView>(id).bindText(property)
+
+fun Activity.bindTextId(id: Int, property: Property<Int>): ListenableSubscription =
+    findViewById<TextView>(id).bindTextId(property)
+
+fun Activity.bindHint(id: Int, property: Property<out CharSequence?>): ListenableSubscription =
+    findViewById<TextView>(id).bindHint(property)
+
+fun Activity.bindHintId(id: Int, property: Property<Int>): ListenableSubscription =
+    findViewById<TextView>(id).bindHintId(property)
+
+fun Activity.bindTextBidirectionally(id: Int, property: MutableProperty<String>): Subscription =
+    findViewById<TextView>(id).bindTextBidirectionally(property)
