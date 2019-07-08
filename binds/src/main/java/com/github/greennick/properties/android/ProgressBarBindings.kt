73c39cf356209229a871e767c5b01b387255b38c
Pavlo Grynyk <paul.grynyk@gmail.com>
2019-07-08 16:40:42 +0300

    - simplify ProgressBar;

--- binds/src/main/java/com/github/greennick/properties/android/ProgressBarBindings.kt
+++ binds/src/main/java/com/github/greennick/properties/android/ProgressBarBindings.kt
@@ -1,12 +1,8 @@
 package com.github.greennick.properties.android
 
-import android.app.Activity
 import android.widget.ProgressBar
 import com.github.greennick.properties.generic.Property
 import com.github.greennick.properties.subscriptions.ListenableSubscription
 
 fun ProgressBar.bindProgress(property: Property<Int>): ListenableSubscription =
     property.subscribe { progress = it }
-
-fun Activity.bindProgress(id: Int, property: Property<Int>): ListenableSubscription =
-        findViewById<ProgressBar>(id).bindProgress(property)
