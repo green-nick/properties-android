0456097550eb9819645133ecc95e38e7f4e3ab25
Pavlo Grynyk <paul.grynyk@gmail.com>
2019-07-29 13:56:59 +0300

    - direct binds as extensions for FragmentActivity and Fragment;

--- bindsx/src/main/java/com/github/greennick/properties/androidx/Helper.kt
+++ bindsx/src/main/java/com/github/greennick/properties/androidx/Helper.kt
@@ -3,26 +3,25 @@ package com.github.greennick.properties.androidx
 import android.app.Activity
 import android.view.View
 import androidx.fragment.app.Fragment
-import java.lang.ClassCastException
 
-inline fun <reified V : View> Activity.find(id: Int): V {
+internal inline fun <reified V : View> Activity.find(id: Int): V {
     val found: View = findViewById(id)
         ?: throw IllegalArgumentException("ID does not reference a View inside this Activity $this")
 
-    return try {
-        found as V
-    } catch (e: ClassCastException) {
+    return if (found is V) {
+        found
+    } else {
         throw ClassCastException("View $found does not correspond required type ${V::class.java}")
     }
 }
 
-inline fun <reified V : View> Fragment.find(id: Int): V {
+internal inline fun <reified V : View> Fragment.find(id: Int): V {
     val found: View = requireView().findViewById(id)
         ?: throw IllegalArgumentException("ID does not reference a View inside this Fragment $this")
 
-    return try {
-        found as V
-    } catch (e: ClassCastException) {
+    return if (found is V) {
+        found
+    } else {
         throw ClassCastException("View $found does not correspond required type ${V::class.java}")
     }
 }
