9808bf4431d37fa4263fb2897b3703d55122e5f7
Pavlo Grynyk <paul.grynyk@gmail.com>
2019-07-29 11:10:37 +0300

    - update view casting messages;

--- bindsx/src/main/java/com/github/greennick/properties/androidx/Helper.kt
+++ bindsx/src/main/java/com/github/greennick/properties/androidx/Helper.kt
@@ -3,14 +3,26 @@ package com.github.greennick.properties.androidx
 import android.app.Activity
 import android.view.View
 import androidx.fragment.app.Fragment
+import java.lang.ClassCastException
 
-fun <V : View> Activity.find(id: Int): V =
-    findViewById(id)
-        ?: throw IllegalArgumentException("ID does not reference a View inside this Activity")
+inline fun <reified V : View> Activity.find(id: Int): V {
+    val found: View = findViewById(id)
+        ?: throw IllegalArgumentException("ID does not reference a View inside this Activity $this")
 
-fun <V : View> Fragment.find(id: Int): V {
-    val found: V? = requireView().findViewById(id)
+    return try {
+        found as V
+    } catch (e: ClassCastException) {
+        throw ClassCastException("View $found does not correspond required type ${V::class.java}")
+    }
+}
 
-    return found
-        ?: throw IllegalArgumentException("ID does not reference a View inside this Activity")
+inline fun <reified V : View> Fragment.find(id: Int): V {
+    val found: View = requireView().findViewById(id)
+        ?: throw IllegalArgumentException("ID does not reference a View inside this Fragment $this")
+
+    return try {
+        found as V
+    } catch (e: ClassCastException) {
+        throw ClassCastException("View $found does not correspond required type ${V::class.java}")
+    }
 }
