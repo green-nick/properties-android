683aad9e110934cc05419bfb71798d9a4873863c
Pavlo Grynyk <paul.grynyk@gmail.com>
2019-08-01 13:16:17 +0300

    - direct boolean binds as extensions for FragmentActivity and Fragment;

--- bindsx/src/main/java/com/github/greennick/properties/androidx/General.kt
+++ bindsx/src/main/java/com/github/greennick/properties/androidx/General.kt
@@ -3,8 +3,7 @@ package com.github.greennick.properties.androidx
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Lifecycle
-import com.github.greennick.properties.generic.Property
-import com.github.greennick.properties.generic.subscribeNonNull
+import com.github.greennick.properties.generic.*
 import com.github.greennick.properties.lifecycle.toEvent
 
 fun <T> FragmentActivity.bind(
@@ -23,6 +22,22 @@ fun <T> FragmentActivity.bindNonNull(
     property.subscribeNonNull(listener).toEvent(this, event)
 }
 
+fun FragmentActivity.bindOnTrue(
+    property: Property<Boolean?>,
+    event: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
+    onTrue: () -> Unit
+) {
+    property.subscribeOnTrue(onTrue).toEvent(this, event)
+}
+
+fun FragmentActivity.bindOnFalse(
+    property: Property<Boolean?>,
+    event: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
+    onFalse: () -> Unit
+) {
+    property.subscribeOnFalse(onFalse).toEvent(this, event)
+}
+
 fun <T> Fragment.bind(
     property: Property<T>,
     event: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
@@ -37,4 +52,20 @@ fun <T> Fragment.bindNonNull(
     listener: (T) -> Unit
 ) {
     property.subscribeNonNull(listener).toEvent(this.viewLifecycleOwner, event)
+}
+
+fun Fragment.bindOnTrue(
+    property: Property<Boolean?>,
+    event: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
+    onTrue: () -> Unit
+) {
+    property.subscribeOnTrue(onTrue).toEvent(this.viewLifecycleOwner, event)
+}
+
+fun Fragment.bindOnFalse(
+    property: Property<Boolean?>,
+    event: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
+    onFalse: () -> Unit
+) {
+    property.subscribeOnFalse(onFalse).toEvent(this.viewLifecycleOwner, event)
 }
