2d3d316bc301348ee5b3de90ff86cf140d1cdc93
greennick <paul.grynyk@gmail.com>
2020-02-06 20:59:25 +0200

    - allow General extensions to receive method refs without setting Lifecycle event explicitly;

--- bindsx/src/main/java/com/github/greennick/properties/androidx/General.kt
+++ bindsx/src/main/java/com/github/greennick/properties/androidx/General.kt
@@ -5,15 +5,31 @@ import androidx.lifecycle.LifecycleOwner
 import com.github.greennick.properties.generic.*
 
 /**
+ * See [bind] with [Lifecycle.Event.ON_DESTROY] event
+ */
+fun <T> LifecycleOwner.bind(
+    property: Property<T>,
+    listener: (T) -> Unit
+): Unit = bind(property, Lifecycle.Event.ON_DESTROY, listener)
+
+/**
  * Connect given listener and property with lifecycle event.
  * Default is [Lifecycle.Event.ON_DESTROY]
  */
 fun <T> LifecycleOwner.bind(
     property: Property<T>,
-    event: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
+    event: Lifecycle.Event,
     listener: (T) -> Unit
 ): Unit = property.subscribe(listener)
     .toEvent(suitableLifecycleOwner(), event)
+
+/**
+ * See [bindNonNull] with [Lifecycle.Event.ON_DESTROY] event
+ */
+fun <T> LifecycleOwner.bindNonNull(
+    property: Property<T?>,
+    listener: (T) -> Unit
+): Unit = bindNonNull(property, Lifecycle.Event.ON_DESTROY, listener)
 
 /**
  * Connect given listener and property with lifecycle event.
@@ -22,10 +38,18 @@ fun <T> LifecycleOwner.bind(
  */
 fun <T> LifecycleOwner.bindNonNull(
     property: Property<T?>,
-    event: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
+    event: Lifecycle.Event,
     listener: (T) -> Unit
 ): Unit = property.subscribeNonNull(listener)
     .toEvent(suitableLifecycleOwner(), event)
+
+/**
+ * See [bindOnTrue] with [Lifecycle.Event.ON_DESTROY] event
+ */
+fun LifecycleOwner.bindOnTrue(
+    property: Property<Boolean?>,
+    onTrue: () -> Unit
+): Unit = bindOnTrue(property, Lifecycle.Event.ON_DESTROY, onTrue)
 
 /**
  * Connect given listener and Property<Boolean?> with lifecycle event.
@@ -38,6 +62,14 @@ fun LifecycleOwner.bindOnTrue(
     onTrue: () -> Unit
 ): Unit = property.subscribeOnTrue(onTrue)
     .toEvent(suitableLifecycleOwner(), event)
+
+/**
+ * See [bindOnFalse] with [Lifecycle.Event.ON_DESTROY] event
+ */
+fun LifecycleOwner.bindOnFalse(
+    property: Property<Boolean?>,
+    onFalse: () -> Unit
+): Unit = bindOnFalse(property, Lifecycle.Event.ON_DESTROY, onFalse)
 
 /**
  * Connect given listener and Property<Boolean?> with lifecycle event.
